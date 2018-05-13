a549edfe86de7d8797a2ae8d6ae8c160a79d6c55
Don Ostergaard <oster012@rangers.uwp.edu>
2018-05-13 18:10:59 -0500

    take my updated class

--- Conversions-N-Stuff/MeasureViewController.swift
+++ Conversions-N-Stuff/MeasureViewController.swift
@@ -8,59 +8,114 @@
 
 import UIKit
 
+// this class will allow us to populate the picker with 2 columns with the right conversion types
+class UnitsConv {
+    var startUnit: String
+    var convertUnit: [String]
+    
+    init(startUnit:String, convertUnit:[String]) {
+        self.startUnit = startUnit
+        self.convertUnit = convertUnit
+    }
+}
+
+
 class MeasureViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource  {
     
-    // All of the conversions allowed in the picker
-    var list = ["Inches To Feet", "Inches to Miles", "Inches To Centimeters", "Inches To Meters","Inches To Kilometers", "Feet To Inches", "Feet To Miles", "Feet To Centimeters", "Feet To Meters", "Feet To Kilometers", "Miles To Inches", "Miles To Feet", "Miles To Centimeters", "Miles To Meters", "Miles To Kilometers", "Centimeters To Inches", "Centimeters To Feet", "Centimeters To Miles", "Centimeters To Meters", "Centimeters To Kilometers", "Meters To Inches", "Meters To Feet", "Meters To Miles", "Meters To Centimeters", "Meters To Kilometers", "Kilometers To Inches", "Kilometers To Feet", "Kilometers To Miles", "Kilometers To Centimeters", "Kilometers To Meters"]
     
-  
-    // user can only add one job at a time
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
-        return 1
+    var units = [UnitsConv]()
+    var UnitPickedConv = ""
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        // setting up all the arrays for all possible conversions between units. Will show in Picker
+        units.append(UnitsConv(startUnit: "Inches", convertUnit: ["Feet", "Yard", "Mile", "Centimeter", "Meter", "Kilometer"]))
+        
+        units.append(UnitsConv(startUnit: "Centimeter", convertUnit: ["Meter", "Kilometer", "Inches", "Feet", "Yard", "Mile"]))
+        
+        units.append(UnitsConv(startUnit: "Feet", convertUnit: ["Inches", "Yard", "Mile", "Centimeter", "Meter", "Kilometer"]))
+        
+        units.append(UnitsConv(startUnit: "Meter", convertUnit: ["Centimeter", "Kilometer", "Inches", "Feet", "Yard", "Mile"]))
+        
+        units.append(UnitsConv(startUnit: "Yard", convertUnit: ["Inches", "Feet", "Mile", "Centimeter", "Meter", "Kilometer"]))
+        
+        units.append(UnitsConv(startUnit: "Mile", convertUnit: ["Inches", "Feet", "Yard", "Centimeter", "Meter", "Kilometer"]))
+        
+        units.append(UnitsConv(startUnit: "Kilometer", convertUnit: ["Centimeter", "Meter", "Inches", "Feet", "Yard", "Mile"]))
+        
+        self.inputField.delegate = self
+        self.picker.isHidden = true
+        self.displayType.isHidden = true
+        // Do any additional setup after loading the view.
     }
     
+    // user can pick staring units and then convert units (picker has 2 columns)
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 2
+        
+    }
     
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
         
-        return list.count
+        
+        if component == 0 {
+            return units.count
+        }
+        else {
+            let selectedUnit = pickerView.selectedRow(inComponent: 0)
+            return units[selectedUnit].convertUnit.count
+        }
         
     }
     
-    // return the job the user has picked from the job list picker obj
+    // get the units and coversion type from the picker once user has selected them
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
+        // let the user pick units by each row independently
         self.view.endEditing(true)
-        //here
-        return list[row]
+        
+        // left column (unit given by user)
+        if component == 0 {
+            return units[row].startUnit
+        }
+            // right column (unit to conver input to)
+        else {
+            let selectedUnit = pickerView.selectedRow(inComponent: 0)
+            return units[selectedUnit].convertUnit[row]
+        }
         
     }
     
     //  save jobs in the appropriate fields
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
+        // here we will save off both rows from the picker. The 1st is the unit to be converted,
+        // the 2nd is what the user wants to convert it to. Then it will be saved into a string and
+        // it will be displayed to the user using the displayType label.
+        pickerView.reloadComponent(1)
+        let selectedUnit = pickerView.selectedRow(inComponent: 0)
+        let selectedConversionUnit = pickerView.selectedRow(inComponent: 1)
+        let givenUnit = units[selectedUnit].startUnit
+        let ConvUnit = units[selectedUnit].convertUnit[selectedConversionUnit]
         
-        //self.taskTextbox.text = self.list[row]
-        //BeginingText = taskTextbox.text!
-        // once the user has picked their task close the menu..
-        self.displayType.text = list[row]
-        self.picker.isHidden = true
+        UnitPickedConv = "\(givenUnit) to \(ConvUnit)"
+        self.displayType.text = UnitPickedConv
+        print(UnitPickedConv)
     }
     
-
+    
     @IBOutlet weak var displayType: UILabel!
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var convertBtn: UIButton!
     @IBOutlet weak var calculateBtn: UIButton!
     @IBOutlet weak var resultField: UITextField!
+    @IBOutlet weak var inputField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.picker.isHidden = true
-        self.displayType.isHidden = true
-        // Do any additional setup after loading the view.
-    }
-
+    
+    // user can pick units and conversion units from the pick. This button pulls up the picker
     @IBAction func convertType(_ sender: UIButton) {
         if sender == convertBtn{
             self.picker.isHidden = false
@@ -68,118 +123,48 @@ class MeasureViewController: UIViewController, UITextFieldDelegate, UIPickerView
         }
     }
     
-    
-    @IBAction func calculate(_ sender: Any) {
+    // Once the user has clicked this button there units will be calculated and displayed on the
+    // textfield. The input textfield must input else throw and alert and don't do the calculation
+    @IBAction func calculate(_ sender: UIButton) {
+        if sender == calculateBtn{
+            self.picker.isHidden = true
+            self.displayType.isHidden = false
+        }
         
+        // dont allow nothing to be inputed
+        if(inputField.text?.isEmpty == true ) {
+            let alert = UIAlertController(title: "Error: Missing field", message: "Please fill out the Input field before a Conversion is Preformed", preferredStyle: .alert)
+            let cancel = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(cancel)
+            present(alert, animated: true, completion: nil)
+            
+            //print("check complete")
+        } else {
+            
+            
+        }
+    }
+    
+    // this will ensure that the user has to use numbers and 1 decimal point
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if (textField == inputField){
+            // only allow digits in the text field
+            let allowedCharacters = CharacterSet(charactersIn:".0123456789").inverted
+            let components = string.components(separatedBy: allowedCharacters)
+            let filtered = components.joined(separator: "")
+            if (textField.text?.contains("."))!, string.contains(".") {
+                return false
+            }
+            return string == filtered
+        }
+        return true
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-/*
-//
-//  TimeConvertorViewController.swift
-//  Conversions-N-Stuff
-//
-//  Created by Don Ostergaard on 5/13/18.
-//  Copyright © 2018 Don Ostergaard. All rights reserved.
-//
-
-import UIKit
-
-class MeasureConvertorViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    @IBOutlet weak var picker: UIPickerView!
-    
-    
-    @IBOutlet weak var pickType: UIButton!
-    
-    // All of the conversions allowed in the picker
-    var list = ["Inches To Feet", "Inches to Miles", "Inches To Centimeters", "Inches To Meters","Inches To Kilometers", "Feet to Inches", "Feet to Miles", "Feet to Centimeters", "Feet to Meters", "Feet to Kilometers", "Miles to Inches", "Miles to Feet", "Miles to Centimeters", "Miles to Meters", "Miles to Kilometers", "Centimeters to Inches", "Centimeters to Feet", "Centimeters to Miles", "Centimeters to Meters", "Centimeters to Kilometers", "Meters to Inches", "Meters to Feet", "Meters to Miles", "Meters to Centimeters", "Meters to Kilometers", "Kilometers to Inches", "Kilometers to Feet", "Kilometers to Miles", "Kilometers to Centimeters", "Kilometers to Meters"]
-    
-    
-    
-    func createDatePicker(){
-        //tool bar for when the date picker shows on screen
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        // done button for toolbar to stop editing or picking a date
-        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
-        toolbar.setItems([done], animated: false)
-    }
-    
-    // when the user presses done after they have choosen a date it locks it in and goes
-    // back to the main screen. IE the date picker is out of view on the screen
-    @objc func donePressed(){
-        self.view.endEditing(true)
-    }
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //createDatePicker()
-        self.picker.isHidden = true
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    
-    // user can only add one job at a time
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
-        return 1
-        
-    }
-    
-    
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-        
-        return list.count
-        
-    }
-    
-    // return the job the user has picked from the job list picker obj
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        
-        self.view.endEditing(true)
-        return list[row]
-        
-    }
-    
-    //  save jobs in the appropriate fields
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-        
-        //self.taskTextbox.text = self.list[row]
-        //BeginingText = taskTextbox.text!
-        // once the user has picked their task close the menu..
-        self.picker.isHidden = true
-    }
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    
     
 }
- */
+
+
